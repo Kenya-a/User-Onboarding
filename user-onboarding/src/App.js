3a5f00d91403569d24c405cdf5e6118505493537
Kenya-a <Kenya-a@users.noreply.github.com>
2019-08-07 14:56:02 -0500

    ready for validation

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Form from './components/Form';
+import InputForm from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <Form/>
-        </p>
+        
+        <InputForm/>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,36 +1,67 @@
 import React, { useState } from 'react';
+import { withFormik, Form, Field} from 'formik';
 
-function Form() {
-    const [user, setUser] = useState({ username: '' })
 
-    const handleChange = event => {
-        setUser({ ...user, [event.target.name]: event.target.value })
-    };
+function InputForm() {
+    //❌❌Remove all previous state management and change/submit handlers
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        console.log(user.username)
-    };
+    // const [user, setUser] = useState({ username: '' })
+
+    // const handleChange = event => {
+    //     setUser({ ...user, [event.target.name]: event.target.value })
+    // };
+
+    // const handleSubmit = event => {
+    //     event.preventDefault();
+    //     console.log(user.username)
+    // };❌❌
 
     return (
         <div className='form'>
-         {console.log(user)}
-           <form onSubmit={event => handleSubmit(event)}>
-               <label>
-                   Username:
-                   <input
+           <form> 
+               <Field
                    type = 'text'
                    name = 'username'
-                   onChange = {event => handleChange(event)}
-                    />
-                </label>
+                   placeholder = 'Username'
+                />
+                <Field
+                   type = 'password'
+                   name = 'password'
+                   placeholder = 'Password'
+                />
+                <Field
+                   type = 'email'
+                   name = 'email'
+                   placeholder = 'Email'
+                />
+               {/* Can this be an input?? */}
+                {/* <Field
+                   type = 'checkbox'
+                   name = 'terms'
+                   placeholder = 'Terms of Service'
+                /> */}
                 <button>Submit!</button>
             </form>
         </div>
     );
-
-
-
 }
 
-export default Form;
+const FormikInputForm = withFormik({
+    mapPropsToValues({username, password, email}){
+
+        return{
+            username: username || '',
+            password: password || '',
+            email: email || '',
+        };
+    },
+    
+    handleSubmit(values) {
+        console.log(values)
+        // Form submission will log here
+    },
+
+})(InputForm)
+
+export default FormikInputForm;
+
