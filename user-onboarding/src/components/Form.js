5e472c4bb493a0b77af91e6b367fe7ed76ddef65
Kenya-a <Kenya-a@users.noreply.github.com>
2019-08-07 15:29:14 -0500

    validation complete

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { withFormik, Form, Field} from 'formik';
 
+import * as Yup from 'yup';
 
-function InputForm() {
-    //❌❌Remove all previous state management and change/submit handlers
 
-    // const [user, setUser] = useState({ username: '' })
-
-    // const handleChange = event => {
-    //     setUser({ ...user, [event.target.name]: event.target.value })
-    // };
-
-    // const handleSubmit = event => {
-    //     event.preventDefault();
-    //     console.log(user.username)
-    // };❌❌
+function InputForm({values, errors, touched}) {
+    // ??Why can just error be passed through??
 
     return (
         <div className='form'>
@@ -24,22 +15,50 @@ function InputForm() {
                    name = 'username'
                    placeholder = 'Username'
                 />
+              <div>
+                 {/* Adding destructured 'error' to prop form. For password...what does the && do.*/}
+                  
+                  {touched.email && errors.password && <p>{errors.password}</p>}
+
                 <Field
                    type = 'password'
                    name = 'password'
                    placeholder = 'Password'
                 />
+              </div>
+
+              <div>
+                  {/* Adding destructured 'error' to prop form. For email*/}
+                  {errors.email && <p>{errors.email}</p>}
+
                 <Field
                    type = 'email'
                    name = 'email'
                    placeholder = 'Email'
                 />
+
+               </div>
+
+               <Field 
+                component = 'select' 
+                name = 'gender'
+                // placeholder = 'Select Gender'??
+               > 
+               <option value = 'female'>Female</option>
+               <option value = 'male'>Male</option>
+               < option value = 'other'>Other</option>
+
+               </Field>
+
                {/* Can this be an input?? */}
-                {/* <Field
+               <label>
+                 <Field
                    type = 'checkbox'
-                   name = 'terms'
-                   placeholder = 'Terms of Service'
-                /> */}
+                   name = 'tos'
+                   checked = {values.tos}
+                 />
+                 Accept Terms of Service
+                </label>
                 <button>Submit!</button>
             </form>
         </div>
@@ -47,15 +66,33 @@ function InputForm() {
 }
 
 const FormikInputForm = withFormik({
-    mapPropsToValues({username, password, email}){
+    mapPropsToValues({username, password, email, tos, gender}){
 
         return{
             username: username || '',
             password: password || '',
             email: email || '',
+            tos: tos || false,
+            gender: gender || 'female'
         };
     },
-    
+
+    //==========Validation Schema goes================
+    // 1. telling Formkik what kind of shap of data it is supposed to be. (FROM TK)
+    // 2. Adding custom error-- add into the arguments for the method!
+
+    validationSchema: Yup.object().shape({
+        email: Yup.string()
+          .email('Email not valid')
+          .required('Email is required'),
+        password: Yup.string()
+          .min(6, 'Password must be longer than 6 characters, at least')
+          .required('Password is required')
+      }),
+
+    //===========End Validation Schema ===============
+
+
     handleSubmit(values) {
         console.log(values)
         // Form submission will log here
